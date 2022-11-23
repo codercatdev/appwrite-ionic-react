fac00adfb6c095b268f3503f70d1be996ffd55c2
Alex Patterson <alex@codingcat.dev>
2022-11-22 20:52:33 -0500

    hardcoding

--- src/config/appwrite.ts
+++ src/config/appwrite.ts
@@ -1,8 +1,8 @@
 import { Account, Avatars, Client, Databases, Storage } from 'appwrite';
 
 const client = new Client()
-    .setEndpoint(process.env.APPWRITE_ENDPOINT || 'http://localhost/v1')
-    .setProject(process.env.APPWRITE_PROJECT_ID || '635ea5a7c4cf314b2b29');
+    .setEndpoint('https://cloud.appwrite.io/v1')
+    .setProject('635fb2b9be68d706f108');
     
 const account = new Account(client);
 const databases = new Databases(client);
